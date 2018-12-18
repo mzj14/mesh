1a04ef138f607a30f21626d9d215e41bb310ef66
mzj14 <mazijun2017@outlook.com>
2018-12-18 14:43:41 -0600

    update rnn bench

--- examples/rnn-bench.py
+++ examples/rnn-bench.py
@@ -17,7 +17,7 @@
 
 This is an illustration, not a good model.
 
-# python rnn-bench.py --hidden_size=200 --train_epochs=3 --epochs_between_evals=3 --log_steps=1 --eval_steps=2000 --mesh_shape="b1:2;b2:2" --layout="hidden_1:b1;hidden_2:b2"
+# python rnn-bench.py --hidden_size=200 --train_epochs=1 --epochs_between_evals=1 --log_steps=1 --eval_steps=2 --mesh_shape="b1:2;b2:2" --layout="hidden_1:b1;hidden_2:b2"
 """
 
 from __future__ import absolute_import
